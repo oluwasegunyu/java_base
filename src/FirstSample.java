0ef956693a4c2c08d22b116f3945815786ba3a3d
yxy <charlay_yu@163.com>
2021-06-20 08:36:26 +0800

    数据类型及大小演示

--- src/FirstSample.java
+++ src/FirstSample.java
@@ -1,16 +1,38 @@
 /**
- * Hello World示例程序
- * 注释，类名规范
+ * JAVA一共有8中基本类型
+ * 4种整型：byte 1字节 short 2字节 int 4字节 long 8字节
+ * 2种浮点型 float 4字节 double 8字节
+ * char 字符型
  * @author yxy
  */
-public class FirstSample{ //标准的类名规范：类名是以大写字母开始的单词采用驼峰命名法，源代码的文件名必须与类名相同。
+public class FirstSample{
     public static void main(String[] args){
-        /*
-        注释有三种
-        第一种可以生成自动文档
-        第二种是行注释
-        第三种是多行注释，多行注释不能嵌套
-         */
-        System.out.println("Hello World!");
+        //整型
+        byte aByte = 12;
+        short aShort = 12345;
+        int aInt = 2123456789;
+        long aLong = 9000000000000000000L; //长整型后面要加L
+
+        System.out.println(aByte + " " + aShort  + " " + aInt + " " + aLong);
+
+        //浮点型
+        float aFloat = 3.400001F;
+        double aDouble = 3.4000000001;
+
+        System.out.println(aFloat + " " + aDouble);
+
+        //char类型
+        //代码点：与一个编码表中的某个字符对应的代码值。
+        //代码单元：每个字符用16位表示，辅助字符用连续的代码单元表示
+        String sentence = "四 is a number";
+        int cpCount = sentence.codePointCount(0, sentence.length());
+        for(int i =0 ; i< cpCount;) {
+            int cp = sentence.codePointAt(i);
+            System.out.print(sentence.charAt(i));
+            if (Character.isSupplementaryCodePoint(cp))
+                i += 2; //辅助字符占用2个代码单元
+            else
+                i++;//普通字符占用一个代码单元
+        }
     }
 }
