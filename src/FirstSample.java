ec8c099aa1cd262dfe38cbb6affd9b161751ebc5
yxy <charlay_yu@163.com>
2021-06-21 12:46:29 +0800

    数学函数

--- src/FirstSample.java
+++ src/FirstSample.java
@@ -1,16 +1,14 @@
 /**
- * Hello World示例程序
- * 注释，类名规范
+ * 数学函数
  * @author yxy
  */
 public class FirstSample{ //标准的类名规范：类名是以大写字母开始的单词采用驼峰命名法，源代码的文件名必须与类名相同。
     public static void main(String[] args){
-        /*
-        注释有三种
-        第一种可以生成自动文档
-        第二种是行注释
-        第三种是多行注释，多行注释不能嵌套
-         */
-        System.out.println("Hello World!");
+        //数学函数包括sqrt，pow，sin, cos,exp等还有PI等常数
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+
+        System.out.println(Math.PI * Math.pow(x,2)); //半径为x的圆的面积
     }
 }
