f415e9e3bc02078e2bf34078e9bd52df8b2f54a1
yxy <charlay_yu@163.com>
2021-06-21 12:40:51 +0800

    变量与运算符

--- src/FirstSample.java
+++ src/FirstSample.java
@@ -1,16 +1,37 @@
+import sun.awt.X11.XSystemTrayPeer;
+
 /**
- * Hello World示例程序
- * 注释，类名规范
+ * 变量与运算符
  * @author yxy
  */
-public class FirstSample{ //标准的类名规范：类名是以大写字母开始的单词采用驼峰命名法，源代码的文件名必须与类名相同。
+public class FirstSample{
+    public static final double CM_PER_INCH = 2.54; //常量
     public static void main(String[] args){
-        /*
-        注释有三种
-        第一种可以生成自动文档
-        第二种是行注释
-        第三种是多行注释，多行注释不能嵌套
-         */
-        System.out.println("Hello World!");
+       double paperWidth = 8.5;
+       System.out.println("paper size in centimeters: " + paperWidth * CM_PER_INCH);
+
+       //boolean运算符
+        //&&与 ||或 !非
+        boolean aBoolean = true;
+        boolean bBoolean = false;
+        if(aBoolean && bBoolean){
+            System.out.println("aBoolean && bBoolean is true");
+        }else{
+            System.out.println("aBoolean && bBoolean is false");
+        }
+
+        if(!bBoolean){
+           System.out.println("!bBoolean is true");
+        }
+
+        //位运算
+        //&与 |或 ^异或 ～非
+        int n = 8;
+        int fourthBitFromRight = (n & 0b1000) / 0b1000;
+        System.out.println("fourthBitFromRight is: " + fourthBitFromRight);
+
+        //>>> 右移高位填充0 >> 右移高位填充符号位
+        fourthBitFromRight = (n & (1 << 3)) >> 3;
+        System.out.println("fourthBitFromRight is: " + fourthBitFromRight);
     }
 }
