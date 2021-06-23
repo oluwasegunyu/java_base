040afa78efa4aa1ee8052c364a952177ea19fe0d
yxy <charlay_yu@163.com>
2021-06-23 21:15:21 +0800

    静态方法完成

--- src/Employee.java
+++ src/Employee.java
@@ -4,15 +4,15 @@ import java.util.GregorianCalendar;
 public class Employee {
 
     private static int nextId = 1;
+
     private String name;
     private double salary;
-    private Date hireDay;
+    private int id;
 
-    public Employee(String n, double s, int year, int month, int day){
+    public Employee(String n, double s){
         name = n;
         salary = s;
-        GregorianCalendar calendar = new GregorianCalendar(year, month-1, day);
-        hireDay = calendar.getTime();
+        id = 0;
     }
 
     public String getName(){
@@ -23,12 +23,18 @@ public class Employee {
         return salary;
     }
 
-    public Date getHireDay(){
-        return hireDay;
+    public int getId()
+    {
+        return id;
     }
 
-    public void raiseSalary(double byPercent){
-        double raise = salary * byPercent /100;
-        salary += raise;
+    public void setId(){
+        id = nextId;
+        nextId++;
     }
+
+    public static int getNextId(){
+        return nextId;
+    }
+
 }
--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -10,21 +10,18 @@ public class EmployeeTest{
         创建三个雇员对象
          */
         Employee[] staff = new Employee[3];
-        staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
-        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
-        staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
-
-        //每人提高百分之五的工资
-        for(Employee e: staff){
-            e.raiseSalary(5);
-        }
+        staff[0] = new Employee("Carl Cracker", 75000);
+        staff[1] = new Employee("Harry Hacker", 50000);
+        staff[2] = new Employee("Tony Tester", 40000);
 
         //输出雇员信息
         for(Employee e: staff){
-            System.out.println("name= " + e.getName() + ", salary= " + e.getSalary() + ", hireDay= " + e.getHireDay());
+            e.setId();
+            System.out.println("name= " + e.getName() + ", salary= " + e.getSalary() + ", id= " + e.getId());
         }
 
         //打印下一个可用ID
         int n = Employee.getNextId();
+        System.out.println("Next available id=" + n);
     }
 }
