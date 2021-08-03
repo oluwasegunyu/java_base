322b832a4c239876af017647c2bc48077fdfccc1
yxy <charlay_yu@163.com>
2021-08-04 07:08:38 +0800

    集合学习2

---
+++ src/shuffle/ShuffleTest.java
@@ -0,0 +1,23 @@
+package shuffle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This program demonstrates the random shuffle and sort algorithms
+ * @version
+ * @Author yxy
+ */
+public class ShuffleTest {
+    public static void main(String... args){
+        var numbers = new ArrayList<Integer>();
+        for (int i = 1;i<=49;i++)
+            numbers.add(i);
+        Collections.shuffle(numbers);
+        List<Integer> winningCombination = numbers.subList(0,6);
+        Collections.sort(winningCombination);
+        System.out.println(winningCombination);
+    }
+}
